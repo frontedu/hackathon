b2b010eba0ffdd176257e2e22d915ef735134470
benj <bneto@computacao.cua.ufmt.br>
2020-08-18 23:11:47 -0300

    criando models e migrando base de dados sqlite

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    tipo = models.CharField(max_length=30)
+    def __str__(self):
+        self.tipo
+
+
+class Endereco(models.Model):
+    rua = models.CharField(max_length=25)
+    bairro = models.CharField(max_length=25)
+    cidade = models.CharField(max_length=15)
+    cep = models.IntegerField()
+
+    def __str__(self):
+        return self.cep
+
+
+class Empresa(models.Model):
+    nome = models.CharField(max_length=25)
+    responsavel = models.CharField(max_length=50)
+    telFixo = models.IntegerField()
+    movel = models.IntegerField()
+    cpf = models.IntegerField(blank=False,unique=True)
+    cnpj = models.IntegerField()
+    tipo = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+    local = models.ForeignKey(Endereco,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.cpf
